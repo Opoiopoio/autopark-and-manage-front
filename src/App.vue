76ad3c6789a95d2dc4f5a8030b2fc96f54b10f15
Вячеслав Елсуков <elsuckov.vyacheslav@gmail.com>
2024-04-16 12:14:03 +0300

    feat: Начал разбираться с sass препроцессором

--- src/App.vue
+++ src/App.vue
@@ -11,3 +11,13 @@ import SideToolbar from './components/SideToolbar.vue'
     <SideToolbar />
   </div>
 </template>
+
+<style lang="scss">
+:root {
+  --main-color: #396ca3;
+  --main-monochrome: lighten(var(--main-color), 5%);
+  --main-darken: #2061b5;
+  --sub-color: #a3396c;
+  --accent-color: #6ca339;
+}
+</style>
--- src/components/SideToolbarHeader.vue
+++ src/components/SideToolbarHeader.vue
@@ -4,11 +4,7 @@
       <h3 class="toolbar-header__item" v-for="item of items">{{ item }}</h3>
     </div>
 
-    <div
-      class="toolbar-header__menu"
-      @mouseleave="onMouseLeave"
-      @mouseover="onMouseOver"
-    >
+    <div class="toolbar-header__menu" @mouseleave="onMouseLeave" @mouseover="onMouseOver">
       <img tabindex="2" :src="menu" alt="menu" @click="onMenuClick" />
       <Transition name="fade">
         <div v-if="menuIsOpened" class="toolbar-header__menu__items">
@@ -27,139 +23,139 @@
 </template>
 
 <script setup lang="ts">
-  import { PropType, ref } from "vue";
-  import { IToolbarHeaderItem } from "../model";
-  import menu from "../assets/menu.svg";
+import { PropType, ref } from 'vue'
+import { IToolbarHeaderItem } from '../model'
+import menu from '../assets/menu.svg'
 
-  // :class="{
-  //           'toolbar-header__menu__items_opened': menuIsOpened,
-  //           'toolbar-header__menu__items_openinig': menuIsOpening,
-  //           'toolbar-header__menu__items_closing': menuIsClosing,
-  //         }"
+// :class="{
+//           'toolbar-header__menu__items_opened': menuIsOpened,
+//           'toolbar-header__menu__items_openinig': menuIsOpening,
+//           'toolbar-header__menu__items_closing': menuIsClosing,
+//         }"
 
-  defineProps({
-    items: Array as PropType<string[]>,
-    menuItems: Array as PropType<IToolbarHeaderItem[]>,
-  });
+defineProps({
+  items: Array as PropType<string[]>,
+  menuItems: Array as PropType<IToolbarHeaderItem[]>,
+})
 
-  const menuIsOpened = ref(false);
+const menuIsOpened = ref(false)
 
-  function onMenuClick() {
-    menuIsOpened.value = !menuIsOpened.value;
-  }
+function onMenuClick() {
+  menuIsOpened.value = !menuIsOpened.value
+}
 
-  function onMouseOver() {
-    menuIsOpened.value = true;
-  }
+function onMouseOver() {
+  menuIsOpened.value = true
+}
 
-  function onMouseLeave() {
-    menuIsOpened.value = false;
-  }
+function onMouseLeave() {
+  menuIsOpened.value = false
+}
 </script>
 
 <style>
-  .fade-enter-active,
-  .fade-leave-active {
-    transition: opacity 0.5s;
-  }
-  .fade-enter,
-  .fade-leave-to {
-    opacity: 0;
-  }
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
 
-  .toolbar-header {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    align-items: center;
+.toolbar-header {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
 
-    gap: 12.5px;
-    padding-bottom: 12.5px;
+  gap: 12.5px;
+  padding-bottom: 12.5px;
 
-    color: var(--ft-dark-color);
+  color: var(--ft-dark-color);
 
-    justify-content: space-between;
+  justify-content: space-between;
 
-    /* color: var(--ft-light-color); */
-  }
+  /* color: var(--ft-light-color); */
+}
 
-  .toolbar-header__items-container {
-    height: 100%;
-    padding: 0 20px;
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    gap: 20px;
-    justify-content: flex-start;
-  }
+.toolbar-header__items-container {
+  height: 100%;
+  padding: 0 20px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 20px;
+  justify-content: flex-start;
+}
 
-  .toolbar-header__item {
-    margin: 0;
-  }
+.toolbar-header__item {
+  margin: 0;
+}
 
-  .toolbar-header__menu {
-    height: 32px;
-    display: inline-block;
-    position: relative;
-  }
+.toolbar-header__menu {
+  height: 32px;
+  display: inline-block;
+  position: relative;
+}
 
-  .toolbar-header__menu > img {
-    height: 100%;
-    cursor: pointer;
-    color: var(--main-color);
-  }
+.toolbar-header__menu > img {
+  height: 100%;
+  cursor: pointer;
+  color: var(--main-color);
+}
 
-  .toolbar-header__menu__items {
-    display: block;
+.toolbar-header__menu__items {
+  display: block;
 
-    position: absolute;
-    top: 100%;
-    right: 0;
+  position: absolute;
+  top: 100%;
+  right: 0;
 
-    background-color: var(--main-color);
-    min-width: 100%;
-    overflow: auto;
-    max-height: 200px;
-    z-index: 400;
+  background-color: var(--main-color);
+  min-width: 100%;
+  overflow: auto;
+  max-height: 200px;
+  z-index: 400;
 
-    border-radius: 0.5rem;
+  border-radius: 0.5rem;
 
-    scrollbar-width: none;
-    -ms-overflow-style: none;
-    overflow: -moz-scrollbars-none;
-  }
+  scrollbar-width: none;
+  -ms-overflow-style: none;
+  overflow: -moz-scrollbars-none;
+}
 
-  /* .toolbar-header__menu__items_opened {
+/* .toolbar-header__menu__items_opened {
   display: block;
 } */
 
-  /* .toolbar-header__menu__items_openinig,
+/* .toolbar-header__menu__items_openinig,
 .toolbar-header__menu__items_closing {
   animation: fade;
   animation-duration: 0.1s;
   animation-fill-mode: forwards;
 } */
 
-  .toolbar-header__menu__items_closing {
-    animation-direction: reverse;
-  }
+.toolbar-header__menu__items_closing {
+  animation-direction: reverse;
+}
 
-  .toolbar-header__menu__items::-webkit-scrollbar {
-    width: 0;
-  }
+.toolbar-header__menu__items::-webkit-scrollbar {
+  width: 0;
+}
 
-  .toolbar-header__menu__item {
-    padding: 10px;
-    cursor: pointer;
-    width: 100%;
-    color: #fff;
-  }
+.toolbar-header__menu__item {
+  padding: 10px;
+  cursor: pointer;
+  width: 100%;
+  color: #fff;
+}
 
-  .toolbar-header__menu__item:hover {
-    background-color: var(--main-monochrome);
-  }
+.toolbar-header__menu__item:hover {
+  background-color: var(--main-monochrome);
+}
 
-  .toolbar-header__menu__item:active {
-    background-color: var(--main-darken);
-  }
+.toolbar-header__menu__item:active {
+  background-color: var(--main-darken);
+}
 </style>
